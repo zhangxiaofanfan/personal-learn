03b1fe9b4fd5ea1b2c240240e319529d28db74b4
zhangxiaofanfan <zhangxiao_fanfan@163.com>
2023-07-05 14:59:30 +0800

    init personal-learn project

---
+++ 01-design-mode/src/main/java/com/zhangxiaofanfan/MainApp.java
@@ -0,0 +1,13 @@
+package com.zhangxiaofanfan;
+
+/**
+ * 学习 设计模式 使用的子项目
+ * 
+ * @date 2023-07-05 10:47:46
+ * @author zhangxiaofanfan
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
