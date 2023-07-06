074461190ede4ad21eb5082ee3716951dc9232d5
zhangxiaofanfan <zhangxiao_fanfan@163.com>
2023-07-06 15:25:07 +0800

    update controller method

--- 02-spring-learn/src/main/java/com/zhangxiaofanfan/Application.java
+++ 02-spring-learn/src/main/java/com/zhangxiaofanfan/Application.java
@@ -2,7 +2,7 @@ package com.zhangxiaofanfan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,7 +13,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/helloworld")
+    @GetMapping("/helloworld")
     public String helloWorld() {
         return "Hello World, Learn Spring!";
     }
